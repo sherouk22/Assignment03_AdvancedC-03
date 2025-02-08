79529efe654bb9ecd72d477a022398a92c121903
sherouk22 <sheroukmohy558@gmail.com>
2025-02-09 00:07:37 +0200

    Assignment03_AdvancedC#03

--- Assignment03_AdvancedC#03/Program.cs
+++ Assignment03_AdvancedC#03/Program.cs
@@ -1,10 +1,101 @@
-﻿namespace Assignment03_AdvancedC_03
+﻿using System.Drawing;
+
+namespace Assignment03_AdvancedC_03
 {//Assignment03_AdvancedC#03
+
+    public delegate int StringMethodsDelegate(string s);
+
     internal class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Part 01:
+            //Try Every Point Covered in the Lecture.
+            #region EX01 :
+
+            //StringMethodsDelegate stringMethodsDelegate;
+
+            //stringMethodsDelegate = StringMethods.GetCountOfUpperCaseChars;
+            //stringMethodsDelegate += StringMethods.GetCountOfLowerCaseChars;
+            //// stringMethodsDelegate -= StringMethods.GetCountOfLowerCaseChars;
+
+            //Delegate[] x = stringMethodsDelegate.GetInvocationList();
+            //foreach (Delegate x2 in x)
+            //{
+            //    Console.WriteLine(x2.Method);
+
+            //}
+
+
+            ////int result = stringMethodsDelegate.Invoke("SDFRTdfrtff");
+            ////Console.WriteLine(result); 
+
+            #endregion
+
+            #region EX02
+
+            int[] number = [ 1, 2, 6, 3, 4, 5 , 8, 7 ];
+            Console.WriteLine(string.Join(" ,", number));
+
+            SortingAlgorithm.BubbleSort(number);
+
+            Console.WriteLine(string.Join(" ,", number));
+
+            #endregion
+
+
+
+
         }
     }
+
+    #region EX01
+    class StringMethods
+    {
+
+        public static int GetCountOfUpperCaseChars(string word)
+        {
+            Console.WriteLine("GetCountOfUpperCaseChars");
+            int count = 0;
+            if (word != null)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (char.IsUpper(word[i]))
+                        count++;
+
+                }
+
+            }
+            return count;
+        }
+
+        public static int GetCountOfLowerCaseChars(string word)
+        {
+            Console.WriteLine("GetCountOfLowerCaseChars");
+            int count = 0;
+            if (word != null)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (char.IsLower(word[i]))
+                        count++;
+
+                }
+
+            }
+            return count;
+        }
+
+
+
+    }
+
+    #endregion
+
+
+
+
+
 }
---
+++ Assignment03_AdvancedC#03/SortingAlgorithm.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03_AdvancedC_03
+{
+    public class SortingAlgorithm
+    {
+
+
+        public static void Swap(ref int x, ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        public static void BubbleSort(int[] arr)
+        {
+            if(arr is  null || arr.Length == 0) 
+                return;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length - 1 - i; j++)
+                    if (arr[j] > arr[i + 1])
+                        Swap(ref arr[j], ref arr[i + 1]);
+
+            }
+
+
+        }
+
+        
+
+
+    }
+}
