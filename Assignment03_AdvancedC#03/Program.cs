b9113a4640d3fc27fce6f2f86dc422c6a7365f65
sherouk22 <sheroukmohy558@gmail.com>
2025-02-09 01:38:19 +0200

    Assignment03_AdvancedC#03

--- Assignment03_AdvancedC#03/Program.cs
+++ Assignment03_AdvancedC#03/Program.cs
@@ -1,13 +1,38 @@
 ﻿using System.Drawing;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Assignment03_AdvancedC_03
 {//Assignment03_AdvancedC#03
 
     public delegate int StringMethodsDelegate(string s);
 
+    public delegate bool ConditionDelegate (int n);
+
+   
+
+
     internal class Program
     {
-        
+
+        #region Get Number 
+
+        public static List<int> FindElements(List<int> numbers , ConditionDelegate condition )
+        {
+            List<int> result = new List<int>();
+            if (numbers is not null && numbers.Count > 0)
+            {
+                foreach (int number in numbers)
+                    if (condition.Invoke(number))
+                        result.Add(number);
+
+            }
+            return result;
+
+        }
+
+
+        #endregion
+
         static void Main(string[] args)
         {
             //Part 01:
@@ -41,6 +66,17 @@ namespace Assignment03_AdvancedC_03
             //SortingAlgorithm.BubbleSort(number , CompareTypes.CompareLessThan);
 
             //Console.WriteLine(string.Join(" ,", number));
+
+            #endregion
+
+            #region EX03 :
+
+            //List<int> numbers =Enumerable.Range(0, 100).ToList();
+
+            //List<int > result = FindElements(numbers , MethodCondition.IsOdd);
+            //Console.WriteLine(string.Join(" ,", result));
+
+
 
             #endregion
 
@@ -96,7 +132,28 @@ namespace Assignment03_AdvancedC_03
 
     #endregion
 
+    #region EX03 
+    class MethodCondition
+    {
+        public static bool IsOdd(int X)
+        {
+            return X % 2 == 1;
+        }
 
+        public static bool IsEven(int X)
+        {
+            return X % 2 == 0;
+        }
+
+        public static bool IsBySeven(int X)
+        {
+            return X % 7 == 0;
+        }
+
+
+    }
+
+    #endregion
 
 
 
